2b25dd9d7cb66ec766836a46a4fdbd62276c09f8
Cynthia Chaves <cynthia_chaves@aol.com>
2024-10-06 16:27:37 -0400

    Completed Lab

--- app.js
+++ app.js
@@ -9,26 +9,25 @@ const connect = async () => {
 
     console.log('\nWelcome to the CRM!\n\nPlease select from the following options:\n\n1.Create Customer\n2.View All Customers\n3.Update Customer\n4.Delete Customer\n5.Quit Application\n')
 
-    let selection = prompt('Input selection # and press Enter twice: ');
+    let selection = parseInt(prompt('Input selection # and press Enter: '));
     
-    await prompt(selection);
-
-    if (Number(selection) === 1) {
+    if (selection === 1) {
       await createCustomer();
     }
-    else if (Number(selection) === 2) {
+    else if (selection === 2) {
       await allCustomers();
     }
-    else if (Number(selection) === 3) {
+    else if (selection === 3) {
       await updateCustomer();
     }
-    else if (Number(selection) === 4) {
+    else if (selection === 4) {
       await deleteCustomer();
     }
-    else if (Number(selection) === 5) {
+    else if (selection === 5) {
       await mongoose.connection.close();
+      console.log('\n***Exit Complete***');
     }
-    else if (Number(selection) <1 || Number(selection) > 5) {
+    else if (selection <1 || selection > 5) {
     console.log('\n**** Only Numbers 1-5 Allowed ****\n**** Please Try Again! ****')
     await connect();
     }
@@ -44,30 +43,28 @@ connect();
 const createCustomer = async () => {
     let namePrompt = prompt("New customer's name: ");
     let agePrompt = prompt("New customer's age: ")
-    await prompt(namePrompt);
-    await prompt(agePrompt);
-    await Customer.create({name:`${namePrompt}`, age: `${agePrompt}`});
+    await Customer.create({name: namePrompt, age: agePrompt});
     console.log(`\nNew Customer Added Successfully:\nName: ${namePrompt}\nAge: ${agePrompt}`);
 };
 
 const allCustomers = async () => {
     const customers = await Customer.find({}, 'name age -_id');
-    console.log('Customer List:\n', customers);
+    console.log('\nCustomer List:\n', customers);
   };
 
 const updateCustomer = async () => {
   const customers = await Customer.find({}, 'name age');
-  console.log('Customer List:\n,', customers);
+  console.log('\nCustomer List:\n', customers);
   let id = prompt('Copy & paste customer ID to update: ');
   let namePrompt = prompt("Customer's new name: ");
   let agePrompt = prompt("Customer's new age: ");
-  let updatedCustomer = await Customer.findByIdAndUpdate(id, {name: `${namePrompt}`, age: `${agePrompt}`}, {new: true});
+  let updatedCustomer = await Customer.findByIdAndUpdate(id, {name: namePrompt, age: agePrompt}, {new: true});
   console.log(`\nCustomer Updated Successfully:\nName: ${updatedCustomer.name}\nAge: ${updatedCustomer.age}\nID: ${updatedCustomer._id}`);
 };
 
 const deleteCustomer = async () => {
   const customers = await Customer.find({}, 'name age');
-  console.log('Customer List:\n', customers);
+  console.log('\nCustomer List:\n', customers);
   let id = prompt('Copy & paste customer ID to delete: ');
   let deletedCustomer = await Customer.findByIdAndDelete(id);
   console.log(`\nCustomer Deleted Successfully:\nName: ${deletedCustomer.name}\nAge: ${deletedCustomer.age}\nID: ${deletedCustomer._id}`);
